89d87e2c1a8921b79b552fbcf3a9b2193e23a2b2
khubaib khan <khankhubaib089@gmail.com>
2024-02-15 09:58:26 +0500

    ""First add Cards"

---
+++ src/components/Cards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Cards() {
+  return (
+    <div>
+      <div className="Mycard">A</div>
+      <div className="Mycard">B</div>
+      <div className="Mycard"></div>
+    </div>
+  )
+}
